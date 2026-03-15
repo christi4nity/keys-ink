9a7e7e819a2640f785c8568ff7fbd3b0056d25be
Christian Verhoeven <cv@christianverhoeven.com>
2026-03-15 12:03:33 -0700

    feat: add Kotlin support to build

---
+++ app/src/main/java/com/keysink/inputmethod/latin/common/ComposedData.kt
@@ -0,0 +1,7 @@
+package com.keysink.inputmethod.latin.common
+
+/**
+ * Bundles composed input data for passing to the native suggestion engine.
+ * Placeholder — will be filled in later.
+ */
+class ComposedData
