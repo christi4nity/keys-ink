292724c6623c7102ca5130043d2a0db42b6b9455
Christian Verhoeven <cv@christianverhoeven.com>
2026-03-15 12:21:02 -0700

    feat: add auto-correct settings toggle

--- app/src/main/java/com/keysink/inputmethod/latin/settings/SettingsValues.java
+++ app/src/main/java/com/keysink/inputmethod/latin/settings/SettingsValues.java
@@ -49,6 +49,7 @@ public class SettingsValues {
     public final boolean mShowNumberRow;
     public final boolean mSpaceSwipeEnabled;
     public final boolean mDeleteSwipeEnabled;
+    public final boolean mAutoCorrectionEnabled;
 
     // From the input box
     public final InputAttributes mInputAttributes;
@@ -91,6 +92,7 @@ public class SettingsValues {
         mShowNumberRow = Settings.readShowNumberRow(prefs);
         mSpaceSwipeEnabled = Settings.readSpaceSwipeEnabled(prefs);
         mDeleteSwipeEnabled = Settings.readDeleteSwipeEnabled(prefs);
+        mAutoCorrectionEnabled = prefs.getBoolean(Settings.PREF_AUTO_CORRECT, true);
     }
 
     public boolean isWordSeparator(final int code) {
